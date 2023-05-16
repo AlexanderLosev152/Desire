f99ac226ac718b4ff2199c536ade801ba5ebffd2
Alexander <losev.pavlovo@mail.ru>
2023-05-16 23:34:07 +0300

    fiк

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-
+  $('.header__btn').on('click', function () {
+    $(".rightside__menu").removeClass("rightside__menu--close");
+  });  
+  $('.rightside__menu-close').on('click', function () {
+    $(".rightside__menu").addClass("rightside__menu--close");
+  })
 })
